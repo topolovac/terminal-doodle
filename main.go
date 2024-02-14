dd23dd8c70d8fbbca938dabf86ea13d4810c48f0
Matej Topolovac <>
2024-02-14 20:07:14 +0100

    chore: installed cli helper package

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := cli.NewApp()
+	app.Name = "tn"
+	app.Usage = "A simple terminal application for writing notes."
+	app.Action = func(c *cli.Context) error {
+		println("Hello from Terminal Doodle app!")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
